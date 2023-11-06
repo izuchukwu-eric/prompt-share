a41221cd86c99fbcecaed00b989ec149bda73651
izuchukwu-eric <onukwubeizu@gmail.com>
2023-11-06 14:42:34 +0100

    delete prompt

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -43,7 +43,21 @@ const MyProfile = () => {
     }
 
     const handleDelete = async (post: IPost) => {
-        
+        const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+
+        if(hasConfirmed) {
+          try {
+            await fetch(`/api/prompt/${post._id.toString()}`, {
+              method: 'DELETE'
+            });
+
+            const filteredPosts = posts.filter((p: any) => p._id !== post._id);
+
+            setPosts(filteredPosts)
+          } catch (error) {
+            console.log(error);
+          }
+        }
     }
   return (
     <Profile 
